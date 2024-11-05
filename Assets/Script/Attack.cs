033f5a7f8c0744d73b8e77b184eecf38ef52351b
NguyenTuanLoc <locntse161090@fpt.edu.vn>
2024-11-05 12:54:02 +0700

    change attack script

--- Assets/Script/Attack.cs
+++ Assets/Script/Attack.cs
@@ -7,30 +7,27 @@ public class Attack : MonoBehaviour
     public int attackDamage = 10;
     public Vector2 knockback = Vector2.zero;
 
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-
-    }
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         Damageble damageble = collision.GetComponent<Damageble>();
 
         if (damageble != null)
         {
-            Vector2 deliveredKnockback = transform.parent.localScale.x > 0 ? knockback : new Vector2(-knockback.x, knockback.y);
+            // Ki?m tra xem transform.parent có null không tr??c khi s? d?ng
+            Vector2 deliveredKnockback = (transform.parent != null && transform.parent.localScale.x > 0)
+                ? knockback
+                : new Vector2(-knockback.x, knockback.y);
 
             bool gotHit = damageble.Hit(attackDamage, deliveredKnockback);
 
             if (gotHit)
+            {
                 Debug.Log(collision.name + " hit for " + attackDamage);
+            }
+        }
+        else
+        {
+            Debug.LogWarning("Collider does not have a Damageble component: " + collision.name);
         }
     }
 }
